f584acf00f19d4b3ef1cdf4f03037748c66ec0c2
Adam Kiel <akkiel@iastate.edu>
2017-08-29 22:24:35 -0500

    Finished PageData Class

--- src/PageData.java
+++ src/PageData.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,7 +16,8 @@ public class PageData {
 	String pageName;
 	String title;
 	String[] h1;
-	Map<String, String> altTags = new HashMap<String, String>();
+	Map<String, String> altTags = new HashMap<String, String>(); // URL - ALT
+	ArrayList<String> urls = new ArrayList<String>();
 
 	/*
 	 * Default Constructor
@@ -35,19 +37,17 @@ public class PageData {
 		h1 = new String[h1Tags.size()];
 		for (int i = 0; i < h1Tags.size(); i++) {
 			System.out.println(h1Tags.get(i));
-		    h1[i] = h1Tags.get(i).text();
+			h1[i] = h1Tags.get(i).text();
 		}
 		// img alt Tags
 		Elements imgTags = page.select("img");
-		//foreach
-		//add to hash (url and alt tag)
-		// String img = link.attr("src");
-		// String alt = link.attr("alt");
-
-	}
-
-	void addAltTag(String imgTitle, String altText) {
-		altTags.put(imgTitle, altText);
-		// System.out.println(altTags.get("dog"));
+		for (int i = 0; i < imgTags.size(); i++) {
+			altTags.put(imgTags.get(i).attr("src"), imgTags.get(i).attr("alt"));
+		}
+		// URLs
+		Elements links = page.select("a");
+		for (int i = 0; i < links.size(); i++) {
+			urls.add(links.get(i).attr("href"));
+		}
 	}
 }
